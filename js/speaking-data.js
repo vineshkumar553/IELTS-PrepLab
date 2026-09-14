3345db612c12feec540c5e35815688adde4227c6
vineshkumar553 <vineshkumaraz1@gmail.com>
2026-09-14 23:28:27 +0500

    Add Speaking page foundation and refine site styling

---
+++ js/speaking-data.js
@@ -0,0 +1,34 @@
+const speakingData = {
+    part1: [
+        {
+            id: "studies",
+            topic: "Studies",
+            questions: [
+                {
+                    id: "studies-01",
+                    question: "What do you enjoy most about your studies?"
+                },
+                {
+                    id: "studies-02",
+                    question: "When do you usually find it easiest to study?"
+                },
+                {
+                    id: "studies-03",
+                    question: "How do you usually prepare for an important exam?"
+                },
+                {
+                    id: "studies-04",
+                    question: "Do you prefer studying alone or with other people?"
+                },
+                {
+                    id: "studies-05",
+                    question: "Is there anything you would like to study in the future?"
+                },
+                {
+                    id: "studies-06",
+                    question: "Has the way you study changed over the last few years?"
+                }
+            ]
+        }
+    ]
+};
